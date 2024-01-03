8733e345f9df322614b1de984713c9ff82915e55
Angelo B <angelobandeira55555@gmail.com>
2024-01-03 15:27:40 -0300

    Métodos para armazenamento e recuperação de Saves adicionados

--- src/Menu/JogoRpg.java
+++ src/Menu/JogoRpg.java
@@ -1,6 +1,10 @@
 package Menu;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.InputMismatchException;
@@ -15,6 +19,7 @@ public class JogoRpg {
 	static Scanner ler = new Scanner(System.in);
 	static BufferedReader espaco = new BufferedReader(new InputStreamReader(System.in));
 	
+	static File arquivoDeSaves = new File("Saves.txt");
 	static int inimigosDerrotados = 0;
 	
 	public static void main(String[] args) {
@@ -62,15 +67,30 @@ public class JogoRpg {
 		        	 break;
 				}
 				
-			}while(procurarInimigos!=2 & Jogador.vivo == true & inimigosDerrotados < 10);
+			}while(procurarInimigos!=2 & Jogador.vivo == true & inimigosDerrotados < 50);
 			
 			System.out.printf(" Você derrotou %s inimigos.", inimigosDerrotados);
 			System.out.println(System.lineSeparator().repeat(6));
 			
-			if(inimigosDerrotados >= 10) {
-				
+			
+			if(inimigosDerrotados >= 10) {                                            /*Caso o numero de inimigos mortos seja superior
+			                                                                            a 10 o jogador vence o jogo*/ 
+				                                                                     
 				System.out.print("\n Você ganhou o jogo!");
 	            System.out.println(System.lineSeparator().repeat(5));
+	            espaco.readLine();
+	            
+	            salvarJogador();
+
+	            System.out.println(System.lineSeparator().repeat(6));
+		        System.out.print(" Ver o resultado de todos ganhadores?");
+		        System.out.print("\n [1]-Sim [2]-Não");
+		        System.out.println(System.lineSeparator().repeat(6));
+		        int verResultados = ler.nextInt();
+		        	
+		        if(verResultados == 1) {
+		        	recuperarJogadores();
+		        }
 			}
 			
 		}catch(IOException e) {
@@ -85,11 +105,12 @@ public class JogoRpg {
 			e.printStackTrace();
 		}
 	}
-    private static Inimigo invocarInimigo() {
+	
+    private static Inimigo invocarInimigo() {                                   //Invoca aleatoriamente um tipo de inimigo
 		
-		int numeroAleatorio = random.nextInt(2,5);
+		int numeroAleatorio = random.nextInt(2,6);
 		
-		Inimigo inimigo;                                                          //Invoca aleatoriamente um tipo de inimigo
+		Inimigo inimigo;                                                          
 		
 		    if(numeroAleatorio%2==0) {
 		    	inimigo = new Minion();
@@ -103,6 +124,7 @@ public class JogoRpg {
 		    
 		return inimigo;
 	}
+    
     private static void agir(Inimigo inimigo) throws IOException {               
     	
 		System.out.print("\n [1]-Atacar ");
@@ -162,31 +184,62 @@ public class JogoRpg {
 		
 		espaco.readLine();
 	}
-    static private void droparPocao() {
-    	
+    
+    private static void droparPocao() {                                         //Pode dropar aleatóriamente uma pocao
+   
     	int aleatorio;
     	aleatorio = random.nextInt(2,5);
     	
-    	if(aleatorio%2 == 0) {                                                   //pode dropar aleatoriamente uma pocao
+    	if(aleatorio%2 == 0) {                                                  
     		
     		Jogador.pocoes++;
     		System.out.print("\n O inimigo dropou uma poção.");
     		System.out.println(System.lineSeparator().repeat(6));
         }
     }
-	
+    
+    private static void salvarJogador() throws IOException {                    //Armazena os dados do jogador em um arquivo
+    	
+    	System.out.println(System.lineSeparator().repeat(6));
+    	System.out.println("Digite seu nome para salvar no Rank: ");
+    	System.out.println(System.lineSeparator().repeat(6));
+    	Jogador.nome = ler.next();
+    	
+    	BufferedWriter escritorDeSaves = new BufferedWriter(new FileWriter(arquivoDeSaves, true));
+    	
+    	escritorDeSaves.write("\n\n Nome:" + Jogador.nome
+    			              + "\n nº inimgos derrotado: " + inimigosDerrotados);
+    	
+    	escritorDeSaves.close();
+    }
+    
+    private static void recuperarJogadores() throws IOException {               //Le os dados de outros jogadores em um arquivo
+    	
+    	BufferedReader leitorDeSaves = new BufferedReader(new FileReader(arquivoDeSaves));
+    	
+    	String save = "";
+    	
+    	while((save = leitorDeSaves.readLine()) != null) {
+    		
+    		System.out.println(save);
+    	}
+    	
+    	leitorDeSaves.close();
+    }
+    
     private static class Jogador{
 		
 		private static int vida = 30;
 	    private static int dano;
 	    private static boolean vivo = true;
 		private static int pocoes = 0;
+		private static String nome;
 		
 		private static void curar() {
 			
 			if(pocoes > 0) {
 				
-				vida += random.nextInt(5,11);
+				vida += random.nextInt(7,13);
 				System.out.print(" Você se curou.");
 			    pocoes--;
 			}else {
@@ -199,7 +252,7 @@ public class JogoRpg {
 		
 		private static void atacar(Inimigo inimigo) {
 			
-			dano= random.nextInt(7,11);
+			dano= (int) ((random.nextInt(6,10)) + (0.15 * vida));
 			
 			inimigo.sofrerDano(dano);
 			System.out.print(" Você realizou um ataque.");
